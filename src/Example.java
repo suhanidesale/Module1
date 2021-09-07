4cfec1ee08911562d89997767aee033b0e088ec7
suhanidesale <deslesuhani02@gmail.com>
2021-09-07 15:12:01 +0530

    java_Module1

--- src/Example.java
+++ src/Example.java
@@ -1,2 +1,9 @@
-public class Example {
+class Example {
+    public static void main(String[] args)
+    {
+        int num = 10;
+        System.out.println("Num = "+num);
+        System.out.println("Hello World !");
+    }
+
 }
